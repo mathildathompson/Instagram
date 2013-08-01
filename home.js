43a31c67a2cb77d790b47989ba5150cbb3318e8f
Mathilda Thompson <mathildathompson288@gmail.com>
2013-08-01 12:08:41 +0100

    Changed the timer so that it is working properly

--- home.js
+++ home.js
@@ -1,26 +1,40 @@
 $(document).ready(function(){
   $('#searchterm').change(function(){
-    // setInterval(function(){
-      console.log('hi');
+      // console.log('hi');
       $('.imagesappend').html('');
       var searchterm = $('#searchterm').val();
-      console.log(searchterm);
+      // console.log(searchterm);
       // var search = searchterm.join('');
       search = searchterm.replace(/\s/g,'');
       // console.log(search);
       console.log(search);
       // var clientid = 83f67d3cb5a84f49a3da093e14852987;
-      $.ajax({
+      getImages(search);
+      setInterval(function(){
+        getImages(search)
+        }
+      ,100000);
+      
+  });
+})
+
+function getImages(search){
+  console.log(search);
+  $.ajax({
         type: "GET", 
         dataType: "jsonp",
         cache: false,
         url: 'https://api.instagram.com/v1/tags/' + search + '/media/recent?client_id=096f5c2bbf2d419285aad92606c867f5',
         success: function(response) {
-          console.log('hi2')
+          console.log(response);
+          $('.imagesappend').html('');
           $.each(response.data, function(index, value){
             var img = $('<img id="image_tag">');
             img.attr('src', value.images.standard_resolution.url);
             $('.imagesappend').append(img);
+            $('#searchterm').on('click', function(){
+              $(this).val('');
+            })
             //console.log(key + ':' + value);
           })
         },
@@ -28,21 +42,7 @@ $(document).ready(function(){
           alert('Error:' + errorType + 'with message' + errorMessage);
         }
       });
-    // },20000);
-      // type: 'POST',
-      // data: {searchterm:searchterm}
-    // })
-  });
-})
+}
 
-
-
-//collecting photos that have been tagged with a specific keyword
 //http://techmonks.net/instagram-using-the-api/
 // url: 'https://api.instagram.com/v1/tags/' + search + '?client_id=096f5c2bbf2d419285aad92606c867f5',
-// response.data[0].images.standard_resolution.url
-// $.ajax({
-//           url: "/home",
-//           type: 'POST',
-//           data: {response:response}
-//         })
